ba3d1f4938590b4347d3e7cbd794edcc413046d4
cyh <2835919524@qq.com>
2025-10-23 14:47:54 +0800

    Include Eigen.

--- include/Core/EntryPoint.h
+++ include/Core/EntryPoint.h
@@ -1,11 +1,34 @@
 #pragma once
 
-int main()
-{
-	MeshDef::Log::Init();
-	MD_CORE_WARN("Log system initialized!");
+#include <iostream>
+#include <Eigen/Dense>
 
-	MeshDef::Application* app = new MeshDef::Application();
-	app->Run();
-	delete app;
+using Eigen::MatrixXd;
+using namespace std;
+
+void testEigen(void) {
+    MatrixXd matrix1(2, 2);
+    matrix1(0, 0) = 3;
+    matrix1(1, 0) = 2;
+    matrix1(0, 1) = -1;
+    matrix1(1, 1) = matrix1(0, 0) + matrix1(0, 1);
+    cout << matrix1 << endl;
 }
+
+int main(void) {
+    cout << "Hello World" << endl;
+    testEigen();
+    getchar();
+    return 0;
+}
+
+
+//int main()
+//{
+//	MeshDef::Log::Init();
+//	MD_CORE_WARN("Log system initialized!");
+//
+//	MeshDef::Application* app = new MeshDef::Application();
+//	app->Run();
+//	delete app;
+//}
